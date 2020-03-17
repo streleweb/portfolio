bab503b7617a6ec0ff686f02e20a533501a547d5
pezi <pezi10002@hotmail.com>
2020-03-17 22:59:13 +0100

    Added about page, text color function, solid footer, responsiveness for aboutpage

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
+const biotext = document.getElementById("text-inside-bio");
 
 // NavItems
 const navItems = document.querySelectorAll(".nav-item");
