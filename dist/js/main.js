12eafe34625c6550a370df1abd4eea7d39843191
pezi <pezi10002@hotmail.com>
2020-06-15 03:20:11 +0200

    BioText-Update & Projects added

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,7 +19,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach((item) => item.classList.add("show"));
 
     //set menu state
     showMenu = true;
@@ -28,9 +28,27 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach((item) => item.classList.remove("show"));
 
     //set menu state
     showMenu = false;
   }
 }
+
+// BIO Text, Alter Update
+function calculate_age(dateOfBirth) {
+  var diff_ms = Date.now() - dateOfBirth.getTime();
+  var age_dt = new Date(diff_ms);
+
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+var alter = calculate_age(new Date(1990, 3, 26));
+
+biotext.innerHTML =
+  "Mein Name ist Peter Strele und ich bin " +
+  alter +
+  " Jahre alt. Nach 12 Jahren als hauptberuflicher Musiker, sehe ich" +
+  "meine berufliche Zukunft als Programmierer. Ich studiere zur Zeit IT mit dem Schwerpunkt Programmieren am IT-Kolleg Imst und" +
+  "möchte mich auf Web-Developement spezialisieren. Ansonsten bewege mich gern in der freien Natur und begeistere mich für Sportarten" +
+  " wie Radfahren, Joggen und Darts.";
